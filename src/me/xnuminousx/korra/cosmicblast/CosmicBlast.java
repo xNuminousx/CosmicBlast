1d99120cb197fa34c74b511aad74a0f6037aabbd
xNuminousx <tylermcguirt3@gmail.com>
2017-09-16 10:20:07 -0500

    Fixed the particles and the ability in general

--- src/me/xnuminousx/korra/cosmicblast/CosmicBlast.java
+++ src/me/xnuminousx/korra/cosmicblast/CosmicBlast.java
@@ -51,7 +51,7 @@ public class CosmicBlast extends AvatarAbility implements AddonAbility {
 		if (player.isDead() || !player.isOnline()) {
 			return;
 		}
-		if (player.isSneaking() && !launched) {
+		if ((player.isSneaking()) && (!launched)) {
 			chargeAnimation();
 		} else {
 			if (!isCharged) {
@@ -69,28 +69,31 @@ public class CosmicBlast extends AvatarAbility implements AddonAbility {
 	private void chargeAnimation() {
 		t += Math.PI / 32;
 		Location loc = player.getLocation();
-		for (double phi = 0; phi <= Math.PI * 2; phi += Math.PI / 1.5) {
-			double x = 0.3D * (Math.PI * 4 - t) * Math.cos(t + phi);
-            double y = 1.5D * (Math.PI * 4 - t);
-            double z = 0.3D * (Math.PI * 4 - t) * Math.sin(t + phi);
-            location.add(x, y, z);
-			ParticleEffect.PORTAL.display(location, 0, 0, 0, 0.1F, 5);
-			ParticleEffect.MOB_SPELL_AMBIENT.display(location, 0, 0, 0, 0.02F, 2);
-			ParticleEffect.DRAGON_BREATH.display(location, 0, 0, 0, 0, 2);
-			ParticleEffect.MAGIC_CRIT.display(location, 0, 0, 0, 0, 5);
-			location.subtract(x, y, z);
-			location.getWorld().playSound(loc, Sound.ENTITY_ELDER_GUARDIAN_AMBIENT, 0.2F, 1);
-		}
 		if (t >= Math.PI * 4) {
-			ParticleEffect.FIREWORKS_SPARK.display(loc, 0, 1, 0, 0.03F, 5);
-			ParticleEffect.END_ROD.display(loc, 0, 0, 0, 0.02F, 2);
+			ParticleEffect.FIREWORKS_SPARK.display(player.getLocation(), 0, 1, 0, 0.03F, 5);
+			ParticleEffect.DRAGON_BREATH.display(player.getLocation(), 0, 0, 0, 0.02F, 2);
 			location = GeneralMethods.getTargetedLocation(player, 1);
 			origin = GeneralMethods.getTargetedLocation(player, 1);
 			direction = GeneralMethods.getTargetedLocation(player, 1).getDirection();
 			isCharged = true;
 			launched = false;
 		}
-		
+		if (isCharged) {
+			loc.getWorld().playSound(loc, Sound.ENTITY_ELDER_GUARDIAN_AMBIENT, 0.2F, 1);
+		} else {
+			for (double phi = 0; phi <= Math.PI * 2; phi += Math.PI / 1.5) {
+				double x = 0.3D * (Math.PI * 4 - t) * Math.cos(t + phi);
+	            double y = 1.5D * (Math.PI * 4 - t);
+	            double z = 0.3D * (Math.PI * 4 - t) * Math.sin(t + phi);
+	            loc.add(x, y, z);
+				ParticleEffect.PORTAL.display(loc, 0, 0, 0, 0.1F, 5);
+				ParticleEffect.MOB_SPELL_AMBIENT.display(loc, 0, 0, 0, 0.02F, 2);
+				ParticleEffect.END_ROD.display(loc, 0, 0, 0, 0, 2);
+				ParticleEffect.MAGIC_CRIT.display(loc, 0, 0, 0, 0, 5);
+				loc.subtract(x, y, z);
+				loc.getWorld().playSound(loc, Sound.ENTITY_ELDER_GUARDIAN_AMBIENT, 0.2F, 1);
+			}
+		}
 	}
 	private void blast() {
 		direction = GeneralMethods.getTargetedLocation(player, 1).getDirection();
@@ -161,7 +164,7 @@ public class CosmicBlast extends AvatarAbility implements AddonAbility {
 	@Override
 	public void load() {
 		ProjectKorra.plugin.getServer().getPluginManager().registerEvents(new CosmicBlastListener(), ProjectKorra.plugin);
-		ProjectKorra.log.info("*" + getAuthor() + "* " + "Successfully loaded " + getName() + "; " + getVersion());
+		ProjectKorra.log.info("Successfully loaded " + getName() + " by " + getAuthor());
 		
 		perm = new Permission("bending.ability.cosmicblast");
 		ProjectKorra.plugin.getServer().getPluginManager().addPermission(perm);
@@ -170,7 +173,7 @@ public class CosmicBlast extends AvatarAbility implements AddonAbility {
 
 	@Override
 	public void stop() {
-		ProjectKorra.plugin.getServer().getLogger().info(getName() + " " + getVersion() + "Developed by " + getAuthor() + " has been disabled!");
+		ProjectKorra.plugin.getServer().getLogger().info(getName() + "disabled.");
 		ProjectKorra.plugin.getServer().getPluginManager().removePermission(this.perm);
 		super.remove();
 
