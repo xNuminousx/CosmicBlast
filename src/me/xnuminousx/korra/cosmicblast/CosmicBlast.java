5f87e6f496e9aa74db54ae16cd7e77e4fe07c0db
Numinous <xnuminousx@gmail.com>
2019-02-13 14:48:22 -0600

    updated to spigot 1.13.2 & pk 1.8.8
    Also changed the permission from being default to requiring permission.

--- src/me/xnuminousx/korra/cosmicblast/CosmicBlast.java
+++ src/me/xnuminousx/korra/cosmicblast/CosmicBlast.java
@@ -73,8 +73,8 @@ public class CosmicBlast extends AvatarAbility implements AddonAbility {
 		t += Math.PI / 32;
 		Location loc = player.getLocation();
 		if (t >= Math.PI * 4) {
-			ParticleEffect.FIREWORKS_SPARK.display(player.getLocation(), 0, 1, 0, 0.03F, 5);
-			ParticleEffect.DRAGON_BREATH.display(player.getLocation(), 0, 0, 0, 0.02F, 2);
+			ParticleEffect.FIREWORKS_SPARK.display(player.getLocation().add(0, 1, 0), 1, 0.2, 0.3, 0.2, 0.03F);
+			ParticleEffect.DRAGON_BREATH.display(player.getLocation(), 1, 0.3, 0.1, 0.3, 0.02F);
 			location = GeneralMethods.getTargetedLocation(player, 1);
 			origin = GeneralMethods.getTargetedLocation(player, 1);
 			direction = GeneralMethods.getTargetedLocation(player, 1).getDirection();
@@ -91,10 +91,10 @@ public class CosmicBlast extends AvatarAbility implements AddonAbility {
 	            double y = 1.5D * (Math.PI * 4 - t);
 	            double z = 0.3D * (Math.PI * 4 - t) * Math.sin(t + phi);
 	            loc.add(x, y, z);
-				ParticleEffect.PORTAL.display(loc, 0, 0, 0, 0.1F, 5);
-				ParticleEffect.MOB_SPELL_AMBIENT.display(loc, 0, 0, 0, 0.02F, 2);
-				ParticleEffect.END_ROD.display(loc, 0, 0, 0, 0, 2);
-				ParticleEffect.MAGIC_CRIT.display(loc, 0, 0, 0, 0, 5);
+				ParticleEffect.PORTAL.display(loc, 5, 0, 0, 0, 0.1F);
+				ParticleEffect.SPELL_MOB_AMBIENT.display(loc, 2, 0, 0, 0, 0.02F);
+				ParticleEffect.END_ROD.display(loc, 2, 0, 0, 0, 0);
+				ParticleEffect.CRIT_MAGIC.display(loc, 5, 0, 0, 0, 0);
 				loc.subtract(x, y, z);
 				loc.getWorld().playSound(loc, Sound.ENTITY_ELDER_GUARDIAN_AMBIENT, 0.2F, 1);
 			}
@@ -103,9 +103,9 @@ public class CosmicBlast extends AvatarAbility implements AddonAbility {
 	private void blast() {
 		direction = GeneralMethods.getTargetedLocation(player, 1).getDirection();
 		location.add(direction);
-		ParticleEffect.MAGIC_CRIT.display(location, 0, 0, 0, 1, 5);
-		ParticleEffect.END_ROD.display(location, 0, 0, 0, 0.05F, 3);
-		ParticleEffect.PORTAL.display(location, 0, 0, 0, 1.5F, 5);
+		ParticleEffect.CRIT_MAGIC.display(location, 5, 0, 0, 0, 1);
+		ParticleEffect.END_ROD.display(location, 3, 0, 0, 0, 0.05F);
+		ParticleEffect.PORTAL.display(location, 5, 0, 0, 0, 1.5F);
 		location.getWorld().playSound(location, Sound.ENTITY_ELDER_GUARDIAN_CURSE, 1, 0.01F);
 		
 		for (Entity e : GeneralMethods.getEntitiesAroundPoint(location, 2.5D)) {
@@ -170,7 +170,7 @@ public class CosmicBlast extends AvatarAbility implements AddonAbility {
 
 	@Override
 	public String getVersion() {
-		return "1.3";
+		return "1.4";
 	}
 
 	@Override
@@ -180,7 +180,7 @@ public class CosmicBlast extends AvatarAbility implements AddonAbility {
 		
 		perm = new Permission("bending.ability.cosmicblast");
 		ProjectKorra.plugin.getServer().getPluginManager().addPermission(perm);
-		perm.setDefault(PermissionDefault.TRUE);
+		perm.setDefault(PermissionDefault.FALSE);
 		
 		ConfigManager.getConfig().addDefault("ExtraAbilities.xNuminousx.CosmicBlast.Cooldown", 5000);
 		ConfigManager.getConfig().addDefault("ExtraAbilities.xNuminousx.CosmicBlast.Range", 20);
